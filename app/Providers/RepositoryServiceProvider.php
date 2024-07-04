40cba28ddaeae850228e1a389cfce25f89acd4a9
xoesae <carlosrogeriocr02@gmail.com>
2024-07-04 20:15:27 -0300

    feat: add repository service provider

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
