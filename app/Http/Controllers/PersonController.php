295737d0baaae19d6b1353b35b678a3d516f73c7
xoesae <carlosrogeriocr02@gmail.com>
2024-07-07 20:24:41 -0300

    feat: implement routes and controller

---
+++ app/Http/Controllers/PersonController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exceptions\PersonNotFoundException;
+use App\Http\Requests\StorePersonRequest;
+use App\Http\Requests\UpdatePeopleRequest;
+use App\Services\PersonService;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class PersonController extends Controller
+{
+    public function __construct(
+        private readonly PersonService $personService,
+    ) {}
+
+    public function index(Request $request): View|Factory|Application
+    {
+        $perPage = $request->input('perPage', 10);
+        $page = $request->input('page', null);
+        $people = $this->personService->listAllPaginated($perPage, $page);
+
+        return view('person.index', compact('people'));
+    }
+
+    public function create(): View|Factory|Application
+    {
+        return view('person.form');
+    }
+
+    public function store(StorePersonRequest $request): RedirectResponse
+    {
+        $data = $request->validated();
+
+        $this->personService->create($data);
+
+        return response()->redirectToRoute('people.index')->with('success');
+    }
+
+    /**
+     * @throws PersonNotFoundException
+     */
+    public function show(int $id): View|Factory|Application
+    {
+        $person = $this->personService->findById($id);
+
+        return view('person.show', compact('person'));
+    }
+
+    /**
+     * @throws PersonNotFoundException
+     */
+    public function edit(int $id): View|Factory|Application
+    {
+        $person = $this->personService->findById($id);
+
+        return view('person.form', compact('person'));
+    }
+
+    public function update(UpdatePeopleRequest $request, int $id): RedirectResponse
+    {
+        $data = $request->validated();
+
+        $this->personService->update($id, $data);
+
+        return response()->redirectToRoute('people.index')->with('success');
+
+    }
+
+    public function destroy(int $id): RedirectResponse
+    {
+        $this->personService->delete($id);
+
+        return response()->redirectToRoute('people.index')->with('success');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\PersonController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pessoas', [PersonController::class, 'index'])->name('people.index');
+Route::get('/pessoas/criar', [PersonController::class, 'create'])->name('people.create');
+Route::get('/pessoas/{id}', [PersonController::class, 'show'])->name('people.show');
+Route::get('/pessoas/{id}/editar', [PersonController::class, 'edit'])->name('people.edit');
+Route::post('/pessoas', [PersonController::class, 'store'])->name('people.store');
+Route::put('/pessoas/{id}', [PersonController::class, 'update'])->name('people.update');
+Route::delete('/pessoas/{id}', [PersonController::class, 'destroy'])->name('people.destroy');
